d31a39c21f959ad6eb686c55ec0d5ffbfb9d15e5
tejasvairal1234 <tejasvairal7066@gmail.com>
2025-12-15 09:29:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
 //your JS code here. If required.
-cy.get('li a')
-  .should('have.css', 'cursor', 'pointer')
-
-cy.get('li').should($li => {
-  expect($li.css('padding')).to.exist
-  expect($li.css('margin')).to.exist
-})
